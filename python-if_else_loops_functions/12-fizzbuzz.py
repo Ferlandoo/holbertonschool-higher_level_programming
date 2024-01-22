ee0356e5b514449889dd4a45e8e5b6c1187c2712
Ferlando <contact@ferlando.me>
2024-01-22 15:50:27 +0100

    edit

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -2,10 +2,9 @@
 def fizzbuzz(num):
     if num % 3 and num % 5:
         print("FizzBuzz", end=" ")
-    elif num % 5:
-        print("Buzz", end=" ")
     elif num % 3:
         print("Fizz", end=" ")
+    elif num % 5:
+        print("Buzz", end=" ")
     else:
         print(num, end=" ")
-         
