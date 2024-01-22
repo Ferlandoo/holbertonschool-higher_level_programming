dde44fdf6be7614876bf2d15a7a355ed55063f9a
Ferlando <contact@ferlando.me>
2024-01-22 13:29:05 +0100

    edit code

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 for i in range(0, 100):
-    if i < 10:
-        i = '0' + str(i)
-        print("{}".format(i), end=', ')
+    if i < 99:
+        print("{:02d}".format(i), end=', ')
     else:
-        print("{}".format(i), end=', ')
+        print("{}".format(i))
