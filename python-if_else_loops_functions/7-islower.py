4e229f56e1f0b81a539dbfb2ded6e9a87919f9d6
Ferlando <contact@ferlando.me>
2024-01-22 15:25:18 +0100

    edit

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -1,6 +1,5 @@
 def islower(c):
-    while True:
-        if ord(c) in range(ord('a'), ord('z') + 1):
-            return True
-        else:
-            return False
+    if ord(c) in range(ord('a'), ord('z') + 1):
+        return True
+    else:
+        return False
