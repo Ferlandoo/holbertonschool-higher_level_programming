0a066e3e8a4ced9e8c74e4c32fd9c87955cfd10a
Ferlando <contact@ferlando.me>
2024-01-24 01:02:28 +0100

    update code

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -2,7 +2,6 @@
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for element in row:
-            print("{}".format(element), end=" ")
             if element != row[-1]:
                 print("{:d}".format(element), end=" ")
             else:
