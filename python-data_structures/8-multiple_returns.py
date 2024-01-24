61e1d747b97fc840c9dde3dcefdc980aa3022797
Ferlando <contact@ferlando.me>
2024-01-24 11:25:36 +0100

    add ex 8

---
+++ python-data_structures/8-multiple_returns.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    tuple_var = (len(sentence), sentence[0])
+    return tuple_var
+
+
+# Write a function that returns a tuple with the length of a string and its first character.
+
+# Prototype: def multiple_returns(sentence):
+# If the sentence is empty, the first character should be equal to None
+# You are not allowed to import any module
