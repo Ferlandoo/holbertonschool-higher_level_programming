3ff1726071a65f0eab500c401fd48598caf30d03
Ferlando <contact@ferlando.me>
2024-01-24 11:41:03 +0100

    update code

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    return len(sentence), sentence[0]
+    if len(sentence) == 0:
+        sentence = "None"
+        return len(sentence), sentence
+    else:
+        return len(sentence), sentence[0]
