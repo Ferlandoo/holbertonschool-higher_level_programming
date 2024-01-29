f8dc6d4ac1d8f56e401f394cf22b230774f9a6f6
Ferlando <contact@ferlando.me>
2024-01-29 20:10:42 +0100

    update code

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -4,8 +4,6 @@ def roman_to_int(roman_string):
     nr = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
     if type(roman_string) != str:
         return 0
-    if roman_string is None:
-        return 0
     for idx in range(len(roman_string)):
         if idx > 0 and nr[roman_string[idx]] > nr[roman_string[idx - 1]]:
             result += nr[roman_string[idx]] - 2 * nr[roman_string[idx - 1]]
