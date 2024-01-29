902625284f3030ba46ad97a3108b4add28fc535e
Ferlando <contact@ferlando.me>
2024-01-29 13:45:01 +0100

    update code

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     if a_dictionary:
-        max_v = max(set(a_dictionary.values()))
-        key = list(filter(lambda x: a_dictionary[x] == max_v, a_dictionary))[0]
-        return key
+        max_value = max(set(a_dictionary.values()))
+        for key, value in a_dictionary.items():
+            if value == max_value:
+                return key
     else:
         return None
