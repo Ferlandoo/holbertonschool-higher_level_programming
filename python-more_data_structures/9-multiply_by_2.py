90e30a372a03f177fa9f874b7ae40f8a96c8f1e8
Ferlando <contact@ferlando.me>
2024-01-29 13:07:57 +0100

    update code

--- python-more_data_structures/9-multiply_by_2.py
+++ python-more_data_structures/9-multiply_by_2.py
@@ -2,11 +2,3 @@
 def multiply_by_2(a_dictionary):
     new_dict = dict((key, value * 2) for key, value in a_dictionary.items())
     return new_dict
-
-
-# Write a function that returns a new dictionary with all values multiplied by 2
-
-# Prototype: def multiply_by_2(a_dictionary):
-# You can assume that all values are only integers
-# Returns a new dictionary
-# You are not allowed to import any module
