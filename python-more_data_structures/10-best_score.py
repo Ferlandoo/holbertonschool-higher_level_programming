477c2294b469de767817406ec24bbfcb9c813638
Ferlando <contact@ferlando.me>
2024-01-29 14:19:10 +0100

    update and add 12

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     if a_dictionary:
-        max_value = max(set(a_dictionary.values()))
+        max_value = max(a_dictionary.values())
         for key, value in a_dictionary.items():
             if value == max_value:
                 return key
