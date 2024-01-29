407bc352287e6ed37a56441466744ea2446dafaa
Ferlando <contact@ferlando.me>
2024-01-29 21:29:17 +0100

    update code

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    try:
-        for i in range(0, x):
-            print(my_list[i], end="")
-    except IndexError:
-        print()
-        return i
-    print()
-    return i + 1
+    elements = 0
+    for idx in range(x):
+        try:
+            print(my_list[idx], end="")
+            elements += 1
+        except IndexError:
+            break
+    print("")
+    return elements
+        
