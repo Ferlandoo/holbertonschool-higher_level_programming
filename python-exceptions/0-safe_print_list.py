7ada87c67d54f9656cbccff328447132a0c1aab0
Ferlando <contact@ferlando.me>
2024-01-29 21:13:15 +0100

    add ex 0

---
+++ python-exceptions/0-safe_print_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    try:
+        for idx in my_list[:x]:
+            print(idx, end="")
+        print()
+        return idx
+    except IndexError:
+        print("Index out of range")
