b8fa63cd55633eb06c0875b53d0e7d19d446f45b
Ferlando <contact@ferlando.me>
2024-01-29 20:08:19 +0100

    update style

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
     result = 0
-    nr = {"I": 1, "V": 5, 
-                     "X": 10, "L": 50, 
-                     "C": 100, "D": 500, 
-                     "M": 1000}
+    nr = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
     if type(roman_string) != str:
         return 0
     if roman_string is None:
