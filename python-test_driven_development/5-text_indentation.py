88e5bbc301ed2ea0c1104f0217333dbc4ce0d4ef
Ferlando <contact@ferlando.me>
2024-02-01 23:03:18 +0100

    update code

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 def text_indentation(text):
-    if not isinstance(text, str):
+    if type(text) is not str:
         raise TypeError("text must be a string")
+    new_text = ""
     for i in range(len(text)):
-        if text[i] == '.' or text[i] == '?' or text[i] == ':':
-            print(text[i])
-            print("")
+        if text[i] == '.' or text[i] == '?' or text[i] == ':' and i != len(text) - 1:
+            new_text += text[i]
+            new_text += "\n\n"
         else:
-            print(text[i], end="")
+            new_text += text[i]
+    print(new_text, end="")
