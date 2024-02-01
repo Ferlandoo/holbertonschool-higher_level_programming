6e53693a95b2e17f1ec5e18b56898beac33fb01e
Ferlando <contact@ferlando.me>
2024-02-01 13:26:11 +0100

    add ex 5

---
+++ python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def text_indentation(text):
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+    for i in range(len(text)):
+        if text[i] == '.' or text[i] == '?' or text[i] == ':':
+            print(text[i])
+            print("")
+        else:
+            print(text[i], end="")
