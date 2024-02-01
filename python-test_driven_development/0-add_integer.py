a5f7aa79a8a5724fa080f85a3cb4eeb9c2f79015
Ferlando <contact@ferlando.me>
2024-02-01 10:49:14 +0100

    update

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,5 +1,17 @@
-#!/usr/bin/python3
 def add_integer(a, b=98):
+    """
+    Adds two numbers and returns the result.
+
+    Parameters:
+    - a (int or float): The first number.
+    - b (int or float, optional): The second number. Defaults to 98 if not provided.
+
+    Returns:
+    int: The sum of the two numbers after rounding them to the nearest integer.
+
+    Raises:
+    TypeError: If either 'a' or 'b' is not an integer or float.
+    """
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer or float")
     elif not isinstance(b, (int, float)):
