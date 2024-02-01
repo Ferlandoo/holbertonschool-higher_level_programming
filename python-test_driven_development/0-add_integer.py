b8c990919ea5fee6ffe51feb8ca3d120d9612562
Ferlando <contact@ferlando.me>
2024-02-01 10:58:36 +0100

    update code

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -10,7 +10,4 @@ def add_integer(a, b=98):
         raise TypeError("a must be an integer or float")
     elif not isinstance(b, (int, float)):
         raise TypeError("b must be an integer or float")
-    else:
-        a = round(a)
-        b = round(b)
-    return a + b
+    return int(a) + int(b)
