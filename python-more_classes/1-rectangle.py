15eafcec6eae960845d1b9204c8f82093efbcecf
Ferlando <contact@ferlando.me>
2024-02-05 11:50:51 +0100

    update code

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -1,13 +1,17 @@
 #!/usr/bin/python3
+"""Module that defines a rectangle"""
+
+
 class Rectangle:
+    """Class that defines a rectangle"""
     def __init__(self, width=0, height=0):
         self.height = height
         self.width = width
-        
+
     @property
     def width(self):
         return self.__width
-    
+
     @width.setter
     def width(self, value):
         if not isinstance(value, int):
@@ -15,11 +19,11 @@ class Rectangle:
         elif value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
-        
+
     @property
     def height(self):
         return self.__height
-    
+
     @height.setter
     def height(self, value):
         if not isinstance(value, int):
