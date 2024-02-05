7f945ea6877ee0f4ae7b62ba3a87b2155c2d234c
Ferlando <contact@ferlando.me>
2024-02-05 11:47:39 +0100

    update code

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
+"""Module that defines a rectangle"""
+
+
 class Rectangle:
     def __init__(self, width=0, height=0):
+        """Class that defines a rectangle"""
         self.width = width
         self.height = height
         
