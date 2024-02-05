d85f80c0b6cb233900e59cad7081ae3dfc1459ba
Ferlando <contact@ferlando.me>
2024-02-05 16:00:36 +0100

    update code

--- python-more_classes/9-rectangle.py
+++ python-more_classes/9-rectangle.py
@@ -56,7 +56,7 @@ class Rectangle:
             return str_1
 
     def __repr__(self):
-        return "Rectangle(f{self.width}, f{self.height})"
+        return f'Rectangle({self.width}, {self.height})'
 
     def __del__(self):
         Rectangle.number_of_instances -= 1
