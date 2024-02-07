09fc3a39df897902ed345f12ae7cc99ac65ae09f
Ferlando <contact@ferlando.me>
2024-02-07 20:20:20 +0100

    update 1

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -3,10 +3,7 @@
 
 
 class MyList(list):
-    def __init__(self):
-        """Initializes the object."""
-        super().__init__()
-        
+    """A class that inherits from list."""
     def print_sorted(self):
         """Prints the list, but sorted."""
         print(sorted(self))
