bf05d7e1c6fbecda870827475ff1dea28a04bf8a
Ferlando <contact@ferlando.me>
2024-02-07 20:48:21 +0100

    update code 4

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -5,6 +5,6 @@
 def inherits_from(obj, a_class):
     """Checks if an object is an instance of a class that inherited from
     the specified class, otherwise False"""
-    if issubclass(type(obj), a_class) and type(obj) != a_class:
+    if issubclass(type(obj), a_class) and type(obj) is not a_class:
         return True
     return False
