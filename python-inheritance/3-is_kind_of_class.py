89faad6681c1953b86c460bc2b89f4f630b85641
Ferlando <contact@ferlando.me>
2024-02-07 20:33:52 +0100

    update 3

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+"""Defines a function that checks if an object is an instance of a class."""
+
+
 def inherits_from(obj, a_class):
+    """Returns True if the object is an instance of a class that inherited"""
     if isinstance(obj, a_class):
         return True
     return False
