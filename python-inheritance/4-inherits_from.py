a1cea924c4ada1802f9a45fc2f3ff6cfca61d5b8
Ferlando <contact@ferlando.me>
2024-02-07 20:44:23 +0100

    update ex 4

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,2 +1,8 @@
+#!/usr/bin/python3
+"""Defines an object attribute lookup function."""
+
+
 def inherits_from(obj, a_class):
+    """Checks if an object is an instance of a class that inherited from
+    the specified class, otherwise False"""
     return issubclass(type(obj), a_class) and type(obj) != a_class
