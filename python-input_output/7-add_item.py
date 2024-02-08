a0051ed50a02a590ea13b81b1e1b0c923a9f3640
Ferlando <contact@ferlando.me>
2024-02-08 13:13:09 +0100

    update comment

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
+"""Name of the JSON file to work with is add_item.json"""
 import sys
+
 
 SaveFile = __import__('5-save_to_json_file')
 LoadFile = __import__('6-load_from_json_file')
