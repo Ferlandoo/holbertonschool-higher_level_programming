7877158f620c75def7333e4de1acc55e3affbe77
Ferlando <contact@ferlando.me>
2024-02-09 00:08:32 +0100

    update style

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -14,10 +14,11 @@ def pascal_triangle(n):
 
         all_lists[0] = [1]
         all_lists[1] = [1, 1]
-        
+
         for idx_all in range(2, n):
             all_lists[idx_all].append(1)
             for element in range(1, idx_all):
-                all_lists[idx_all].append(all_lists[idx_all-1][element-1] + all_lists[idx_all-1][element])
+                all_lists[idx_all].append(all_lists[idx_all-1][element-1] +
+                                          all_lists[idx_all-1][element])
             all_lists[idx_all].append(1)
     return all_lists
