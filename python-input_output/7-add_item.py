c2d982df96dfc7bd731d66168c6122b440631a83
Ferlando <contact@ferlando.me>
2024-02-08 14:11:39 +0100

    update

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -10,7 +10,7 @@ LoadFile = __import__('6-load_from_json_file')
 filename = "add_item.json"
 try:
     my_list = LoadFile.load_from_json_file(filename)
-except:
+except Exception:
     my_list = []
 for idx in range(1, len(sys.argv)):
     my_list.append(sys.argv[idx])
