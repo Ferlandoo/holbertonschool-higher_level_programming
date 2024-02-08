3409e1d6b90a2f24cba2a839611c349408a91713
Ferlando <contact@ferlando.me>
2024-02-08 13:21:29 +0100

    update code

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -10,6 +10,6 @@ LoadFile = __import__('6-load_from_json_file')
 filename = "add_item.json"
 my_list = []
 my_list = LoadFile.load_from_json_file(filename)
-for idx in range(1, len(sys.argv)):
-    my_list.append(sys.argv[idx])
+for i in range(1, len(sys.argv)):
+    my_list.append(sys.argv[i])
 SaveFile.save_to_json_file(my_list, filename)
