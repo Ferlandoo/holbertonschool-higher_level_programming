c2d91705081e9bf22f067bea3185eb870fe35335
Ferlando <contact@ferlando.me>
2024-02-08 11:59:46 +0100

    add ex 6

---
+++ python-input_output/6-load_from_json_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+import json
+"""Definition of the function load_from_json_file"""
+
+
+def load_from_json_file(filename):
+    """Loads a Python object from a JSON file."""
+    with open(filename, "r") as file:
+        return json.load(file)
