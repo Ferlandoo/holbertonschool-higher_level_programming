1440904ea93b9aa975d7e08ffa6f605abfbd084e
Ferlando <contact@ferlando.me>
2024-02-13 00:05:53 +0100

    add ex 15

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 '''Base class'''
-
+import json
 
 class Base:
     '''Base class'''
@@ -14,4 +14,7 @@ class Base:
             self.id = Base.__nb_objects
 
     def to_json_string(list_dictionaries):
-        
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+        else:
+            return json.dumps(list_dictionaries)
