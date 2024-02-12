f50b9c8b3713c59fcf76309bb9afee0bc80de3e2
Ferlando <contact@ferlando.me>
2024-02-13 00:12:35 +0100

    update

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -17,6 +17,6 @@ class Base:
     @staticmethod
     def to_json_string(list_dictionaries):
         '''Return the JSON string representation of list_dictionaries'''
-        if list_dictionaries is None or list_dictionaries == []:
+        if list_dictionaries is None or len(list_dictionaries) == 0:
             return "[]"
         return json.dumps(list_dictionaries)
