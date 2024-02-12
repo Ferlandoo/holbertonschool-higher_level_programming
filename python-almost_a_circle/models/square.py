0e51eeec0d78fc2095b54e9daab5d4c3bf94126e
Ferlando <contact@ferlando.me>
2024-02-12 16:23:23 +0100

    update code

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -7,7 +7,8 @@ class Square(Rectangle):
     def __init__(self, size, x=0, y=0, id=None):
         '''Constructor'''
         super().__init__(size, size, x, y, id)
+        self.size = size
 
     def __str__(self):
         '''String representation'''
-        return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
+        return f"[Square] ({self.id}) {self.x}/{self.y} - {self.size}"
