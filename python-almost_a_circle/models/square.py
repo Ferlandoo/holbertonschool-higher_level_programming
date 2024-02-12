cc2b16fe29bb252a1a75bfadce904db205d35a73
Ferlando <contact@ferlando.me>
2024-02-12 16:47:34 +0100

    update code

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -1,16 +1,14 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 '''Class Square inherits from Rectangle'''
 from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
     '''Class Square'''
-
     def __init__(self, size, x=0, y=0, id=None):
         '''Constructor'''
         super().__init__(size, size, x, y, id)
 
     def __str__(self):
         '''String representation'''
-        return '[{}] ({}) {}/{} - {}'.\
-            format(type(self).__name__, self.id, self.x, self.y, self.width)
+        return f"[{type(self).__name__}] ({self.id}) {self.x}/{self.y} - {self.width}"
