0512fe81a93fb92c2da43b3113903da597e192e6
Ferlando <contact@ferlando.me>
2024-02-12 13:16:38 +0100

    update

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -19,9 +19,9 @@ class Rectangle(Base):
     @width.setter
     def width(self, new_width):
         if not isinstance(new_width, int):
-            raise TypeError(f"width must be an integer")
+            raise TypeError("width must be an integer")
         elif new_width <= 0:
-            raise ValueError(f"width must be > 0")
+            raise ValueError("width must be > 0")
         self.__width = new_width
 
     @property
@@ -31,9 +31,9 @@ class Rectangle(Base):
     @height.setter
     def height(self, new_height):
         if not isinstance(new_height, int):
-            raise TypeError(f"height must be an integer")
+            raise TypeError("height must be an integer")
         elif new_height <= 0:
-            raise ValueError(f"height must be > 0")
+            raise ValueError("height must be > 0")
         self.__height = new_height
 
     @property
@@ -43,9 +43,9 @@ class Rectangle(Base):
     @x.setter
     def x(self, new_x):
         if not isinstance(new_x, int):
-            raise TypeError(f"x must be an integer")
+            raise TypeError("x must be an integer")
         elif new_x < 0:
-            raise ValueError(f"x must be >= 0")
+            raise ValueError("x must be >= 0")
         self.__x = new_x
 
     @property
@@ -55,7 +55,7 @@ class Rectangle(Base):
     @y.setter
     def y(self, new_y):
         if not isinstance(new_y, int):
-            raise TypeError(f"y must be an integer")
+            raise TypeError("y must be an integer")
         elif new_y < 0:
-            raise ValueError(f"y must be >= 0")
+            raise ValueError("y must be >= 0")
         self.__y = new_y
