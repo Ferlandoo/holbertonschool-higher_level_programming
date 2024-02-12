d3321c4cd40ce492389ba2a7d5679da51e70b837
Ferlando <contact@ferlando.me>
2024-02-12 14:02:17 +0100

    add ex 7

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -66,7 +66,10 @@ class Rectangle(Base):
 
     def display(self):
         '''Print Rectangle with #'''
+        for _ in range(self.__y):
+            print()
         for _ in range(self.height):
+            print(" " * self.__x, end="")
             print("#" * self.width)
 
     def __str__(self):
