ebf54f3cedd5aa7fec6764788576572331bd106d
Ferlando <contact@ferlando.me>
2024-02-12 13:42:23 +0100

    add ex 5

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -63,3 +63,8 @@ class Rectangle(Base):
     def area(self):
         '''Area of Rectangle'''
         return self.__width * self.__height
+
+    def display(self):
+        '''Print Rectangle with #'''
+        for _ in range(self.height):
+            print("#" * self.width)
