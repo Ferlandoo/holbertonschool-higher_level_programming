c54a1cbf6656a46520eea958297c01075e622c1a
Ferlando <contact@ferlando.me>
2024-02-12 13:05:09 +0100

    add ex 3

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -18,6 +18,10 @@ class Rectangle(Base):
 
     @width.setter
     def width(self, new_width):
+        if not isinstance(new_width, int):
+            raise TypeError(f"width must be an integer")
+        elif new_width <= 0:
+            raise ValueError(f"width must be > 0")
         self.__width = new_width
 
     @property
@@ -26,6 +30,10 @@ class Rectangle(Base):
 
     @height.setter
     def height(self, new_height):
+        if not isinstance(new_height, int):
+            raise TypeError(f"height must be an integer")
+        elif new_height <= 0:
+            raise ValueError(f"height must be > 0")
         self.__height = new_height
 
     @property
@@ -34,6 +42,10 @@ class Rectangle(Base):
 
     @x.setter
     def x(self, new_x):
+        if not isinstance(new_x, int):
+            raise TypeError(f"x must be an integer")
+        elif new_x < 0:
+            raise ValueError(f"x must be >= 0")
         self.__x = new_x
 
     @property
@@ -42,4 +54,8 @@ class Rectangle(Base):
 
     @y.setter
     def y(self, new_y):
+        if not isinstance(new_y, int):
+            raise TypeError(f"y must be an integer")
+        elif new_y < 0:
+            raise ValueError(f"y must be >= 0")
         self.__y = new_y
