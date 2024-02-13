bd0543dfd4e5e93cc43b49e10b5aec9bb645698a
Ferlando <contact@ferlando.me>
2024-02-13 16:21:46 +0100

    update code

--- python-almost_a_circle/tests/test_models/test_square.py
+++ python-almost_a_circle/tests/test_models/test_square.py
@@ -69,7 +69,7 @@ class TestSquare(unittest.TestCase):
         """check one string"""
         Base._Base__nb_objects = 0
         with self.assertRaisesRegex(TypeError, "width must be an integer"):
-            Square("1", "2", "3", "4")
+            Square("1")
 
     def test_getterAndSetter(self):
         """check getter and setter"""
