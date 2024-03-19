e219d695214f921db04b4fa889b570e9ff188674
Ferlando <contact@ferlando.me>
2024-03-19 17:07:56 +0100

    update

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -8,7 +8,8 @@ if __name__ == "__main__":
     """List all states from the database hbtn_0e_0_usa"""
     db = MySQLdb.connect(
         host="localhost",
-        port=3306, user=argv[1],
+        port=3306,
+        user=argv[1],
         passwd=argv[2],
         db=argv[3])
     cur = db.cursor()
--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -8,7 +8,8 @@ if __name__ == "__main__":
     """List all states with a name starting with N"""
     db = MySQLdb.connect(
         host="localhost",
-        port=3306, user=argv[1],
+        port=3306,
+        user=argv[1],
         passwd=argv[2],
         db=argv[3])
     cur = db.cursor()
