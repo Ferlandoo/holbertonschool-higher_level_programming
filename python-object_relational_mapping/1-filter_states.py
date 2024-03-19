8fe5913857616e5d0eff952126d66d3bf8ac7f8b
Ferlando <contact@ferlando.me>
2024-03-19 17:18:18 +0100

    update

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -11,8 +11,7 @@ if __name__ == "__main__":
         port=3306,
         user=argv[1],
         passwd=argv[2],
-        db=argv[3]
-    )
+        db=argv[3])
     cur = db.cursor()
     cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id")
     rows = cur.fetchall()
