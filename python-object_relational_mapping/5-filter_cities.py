1c592542ade48bb239ab9520f63717fea14afd81
Ferlando <contact@ferlando.me>
2024-03-20 15:28:39 +0100

    add ex 5

---
+++ python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Lists all cities from the database hbtn_0e_4_usa."""
+
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=argv[1],
+        passwd=argv[2],
+        db=argv[3],
+        charset="utf8")
+    cur = db.cursor()
+    cur.execute((
+        "SELECT cities.name"
+        "FROM cities"
+        "JOIN states ON states.id = cities.state_id"
+        "WHERE states.name = 'Texas'"
+        "ORDER BY cities.id"), argv[4])
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
