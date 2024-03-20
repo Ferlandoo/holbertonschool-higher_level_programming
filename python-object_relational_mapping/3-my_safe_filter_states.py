2c7008f4cd56d72c1302bbe1628cb556f83d4725
Ferlando <contact@ferlando.me>
2024-03-20 12:58:55 +0100

    update

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -13,10 +13,9 @@ if __name__ == "__main__":
         port=3306,
         user=argv[1],
         passwd=argv[2],
-        db=argv[3],
-        charset="utf8")
+        db=argv[3])
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE BINARY %s", (argv[4],))
+    cur.execute("SELECT * FROM states WHERE name LIKE BINARY %s", (argv[4], ))
     rows = cur.fetchall()
     for row in rows:
         print(row)
