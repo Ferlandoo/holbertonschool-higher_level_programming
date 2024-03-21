59a76e46cd7681b32f19fd7b189e89ded4af7de1
Ferlando <contact@ferlando.me>
2024-03-21 13:03:07 +0100

    update and add ex 14

---
+++ python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Prints all City objects from the database hbtn_0e_14_usa."""
+
+from sys import argv
+from sqlalchemy import create_engine
+from model_city import Base, City
+from sqlalchemy.orm import Session
+
+if __name__ == "__main__":
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1], argv[2], argv[3])
+    )
+    Base.metadata.create_all(engine)
+    session = Session(engine)
+    query = session.query(City).order_by(City.id).all()
+    if query is not None:
+        for element in query:
+            print(f"{element.state.name}: ({element.id}) {element.name}")
+    session.commit()
+    session.close()
--- python-object_relational_mapping/model_city.py
+++ python-object_relational_mapping/model_city.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
 """Start link class to table in database."""
 
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
-
-class State(Base):
-    """Class State with attributes id and name."""
-    __tablename__ = 'states'
+class City(Base):
+    __tablename__ = 'cities'
     id = Column(Integer, primary_key=True, autoincrement=True)
     name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
