a300b1175dd28581474b2bd7b6dea89fee8db284
Ferlando <contact@ferlando.me>
2024-03-21 12:49:56 +0100

    update chmod and comm

--- python-object_relational_mapping/13-model_state_delete_a.py
+++ python-object_relational_mapping/13-model_state_delete_a.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Adds the State object “Louisiana” to the database hbtn_0e_6_usa"""
+"""Deletes all State objects with a name containing the letter a"""
 
 from sys import argv
 from sqlalchemy import create_engine
