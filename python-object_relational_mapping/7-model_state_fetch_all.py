7bf2430080f63c0dd49c9c45b01c0aa99a2c08a4
Ferlando <contact@ferlando.me>
2024-03-21 11:16:42 +0100

    add ex 7

---
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+""""""
+
+from sys import argv
+from sqlalchemy import create_engine
+from model_state import Base, State
+from sqlalchemy.orm import Session
+
+
+engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(argv[0], argv[1], argv[2]))
+Base.metadata.create_all(engine)
+session = Session(engine)
+for state in session.query(State).order_by(State.id).all():
+    print("{}: {}".format(state.id, state.name))
+session.close()
