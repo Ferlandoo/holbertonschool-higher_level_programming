7941101f1bad752fb726982d219045b5905d7062
Ferlando <contact@ferlando.me>
2024-03-21 11:30:56 +0100

    update

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -6,10 +6,12 @@ from sqlalchemy import create_engine
 from model_state import Base, State
 from sqlalchemy.orm import Session
 
-"""This script should take 3 arguments: mysql username, mysql password and database name."""
-engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(argv[0], argv[1], argv[2]))
-Base.metadata.create_all(engine)
-session = Session(engine)
-for state in session.query(State).order_by(State.id).all():
-    print("{}: {}".format(state.id, state.name))
-session.close()
+if __name__ == "__main__":
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'.format(argv[0], argv[1], argv[2])
+    )
+    Base.metadata.create_all(engine)
+    session = Session(engine)
+    for state in session.query(State).order_by(State.id).all():
+        print("{}: {}".format(state.id, state.name))
+    session.close()
